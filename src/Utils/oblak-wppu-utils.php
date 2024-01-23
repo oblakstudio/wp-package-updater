af93156b009154cb59a0378a94466f87afec96a3
Sibin Grasic <sibin.grasic@oblak.studio>
2024-01-23 08:16:46 +0100

    fix: Function signature

--- src/Utils/oblak-wppu-utils.php
+++ src/Utils/oblak-wppu-utils.php
@@ -5,15 +5,13 @@
  * @package Package Updater
  */
 
-use Oblak\WP\Updater\Interfaces\Package_Handler_Interface;
-
 /**
  * Register a new package handler
  *
- * @param  string                    $hostname Hostname for the handler.
- * @param  Package_Handler_Interface $handler  Handler instance.
+ * @param  string       $hostname Hostname for the handler.
+ * @param  class-string $handler  Handler instance.
  */
-function wppu_register_handler( string $hostname, Package_Handler_Interface $handler ): void {
+function wppu_register_handler( string $hostname, string $handler ): void {
     add_filter(
         'wp_package_handlers',
         fn( $h ) => array_merge(
